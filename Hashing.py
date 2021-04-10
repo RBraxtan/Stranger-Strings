2382f8fb2363743da537d5a3d155bf8ff7057f6e
D-Rational <73146944+D-Rational@users.noreply.github.com>
2021-04-10 11:33:49 -0700

    Add files via upload

--- Hashing.py
+++ Hashing.py
@@ -15,15 +15,23 @@ with open(your_file, 'rb') as f: #read file bytes
     fb = f.read (BLOCK_SIZE) #read file and take in only declared amount
     while len(fb) > 0: #if data is still being read from file
         md5_hash.update(fb)
+        sha1_hash.update(fb)
+        sha256_hash.update(fb)
+        sha512_hash.update(fb)
         fb = f.read(BLOCK_SIZE)#read next block of file
 
 # print different hash formats of the file 
+
+print ("MD5 of Your File:")
 print (md5_hash.hexdigest())
 
+print ("SHA1 of Your File:")
 print (sha1_hash.hexdigest())
 
+print ("SHA256 of Your File:")
 print (sha256_hash.hexdigest())
 
+print ("SHA512 of Your File:")
 print (sha512_hash.hexdigest())
 
 
