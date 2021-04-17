98a29ffb48e3f755b74240081dd05286a42a1ee1
RBraxtan <rileybraxtan@gmail.com>
2021-04-17 10:42:43 -0700

    regex + removed case sensitivity of IOC strings

--- Hashing.py
+++ Hashing.py
@@ -30,20 +30,20 @@ with open(your_file, 'rb') as f: #read file bytes
 
 # print different hash formats of the file 
 
-print ("MD5 of Your File:")
+print ("\nMD5 of Your File:")
 print (md5_hash.hexdigest())
 
-print ("SHA1 of Your File:")
+print ("\nSHA1 of Your File:")
 print (sha1_hash.hexdigest())
 
-print ("SHA256 of Your File:")
+print ("\nSHA256 of Your File:")
 print (sha256_hash.hexdigest())
 
-print ("SHA512 of Your File:")
+print ("\nSHA512 of Your File:")
 print (sha512_hash.hexdigest())
 
 
-print("\n\n")
+print("\n")
 os.system('\".\\dependencies\\sigcheck.exe ' + your_file)
 
 
--- StringDumper.py
+++ StringDumper.py
@@ -1,4 +1,5 @@
 import sys,os,math,hashlib
+import re
 
 if len(sys.argv) < 2:
     print("Syntax Error: StringDumper.py <file_name>")
@@ -36,13 +37,22 @@ else:
         entropytotal += entropy
         string_count += 1
 
+        if re.search("^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$", string):
+            Flagged_Strings.write("Possible Bitcoin address: " + string)
+
+        if re.search("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}", string):
+            Flagged_Strings.write("Possible IPv4 address: " + string)
+
+        #if re.search("(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))", string):
+        #    Flagged_Strings.write("Possible IPv6 address: " + string)
+
         for line in IOCBlacklist:
-            if line.strip("\n") in string:
+            if line.strip("\n").lower() in string.lower():
                 Flagged_Strings.write(line.strip("\n") + ":" + string)
         IOCBlacklist.seek(0)
 
     if string_count != 0:
-        print("Average Entropy: " + str(round(entropytotal,2)) + "/" + str(string_count) + " (" + str(round(entropytotal/string_count, 2)) + ")")
+        print("Average Entropy: " + str(round(entropytotal,2)) + "/" + str(string_count) + " (" + str(round(entropytotal/string_count, 2)) + ")\n")
         
     entropyfile.close()
     os.system("sort /r ./outputs/" + sha256_hash.hexdigest() + "/entropy.txt /O ./outputs/" + sha256_hash.hexdigest() + "/entropy.txt")
